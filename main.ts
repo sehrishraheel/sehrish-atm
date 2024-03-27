f2cd4597b4b13e8962b8e89cd1811dae2b63df3d
sehrishraheel <sehrishm322@gmail.com>
2024-03-28 01:17:28 +0500

    first  commit

---
+++ main.ts
@@ -0,0 +1,83 @@
+#! /user/bin/env node
+import inquirer from "inquirer";
+
+let myBalance = 10000; // Dollars
+
+let myPin = 1234
+
+let pinAnswer = await inquirer.prompt(
+   [
+    {
+        name: "pin",
+        message: "enter your pin",
+        type: "number"
+
+    }
+    ]
+);
+
+if (pinAnswer.pin === myPin) 
+    console.log("correct pin code!!!");
+
+   let operationAns = await inquirer.prompt(
+        [
+            {
+                name: "operation",
+                message: "please select option",
+                type: "list",
+                choices: ["withdraw", "check balance", "fast cash"]
+                
+            }
+        ]
+    );
+
+    console.log(operationAns);
+
+    if (operationAns.operation === "withdraw"){
+        let amountAns = await inquirer.prompt(
+            [
+                {
+                    name: "amount",
+                    message: "enter your amount",
+                    type: "number"
+                }
+            ]
+        );
+
+         myBalance -= amountAns.amount
+
+         console.log("Your remaining balance is: " + myBalance)
+
+                 //check balance
+    } else if (operationAns.operation === "check balance"){
+
+        console.log("your balance is: " + myBalance)
+    
+    } 
+               //fast cash
+    
+    if (operationAns.operation === "fast cash"){
+        let fastCash = await inquirer.prompt([
+                {
+                    name: "cash",
+                    message: "enter your amount",
+                    type: "list",
+                    choices: [1000, 2000, 3000, 4000]
+
+            
+                }
+            ]);
+
+        
+    if(myBalance >= fastCash.cash){
+     myBalance -= fastCash.cash;
+
+     console.log("your remainig balance is:" + myBalance);
+    }
+    else{
+        console.log("please charge your account");
+      }  
+    
+    }else{
+    console.log("Incorrect pin number");
+}
